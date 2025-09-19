c2ce516cce2ce070b4298e10a4f2f95808f3b939
Nicole <x0obubbleso0x@gmail.com>
2025-09-19 10:17:19 -0500

    lots of stuff

--- Assets/Scripts/FrogController.cs
+++ Assets/Scripts/FrogController.cs
@@ -4,10 +4,19 @@ using System.Collections;
 
 public class FrogController : MonoBehaviour
 {
+    Frogger_InputActions _actions;
+
     [SerializeField] Animator _anim;
     public float moveTime = 0.15f;
     [SerializeField] private bool canMove;
+    [SerializeField] private bool isDead;
+    [SerializeField] private bool onRiver;
+    [SerializeField] private bool onPlatform;
 
+    private void Awake()
+    {
+        _actions = new Frogger_InputActions();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,6 +24,15 @@ public class FrogController : MonoBehaviour
         _anim = GetComponentInChildren<Animator>();
     }
 
+    private void OnEnable()
+    {
+        _actions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _actions.Disable();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -23,22 +41,22 @@ public class FrogController : MonoBehaviour
 
     void PlayerUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (_actions.Player.Up.triggered)
         {
             transform.rotation = Quaternion.Euler(0,0,0);
             PlayerMove(Vector2.up);
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (_actions.Player.Down.triggered)
         {
             transform.rotation = Quaternion.Euler(0, 0, 180);
             PlayerMove(Vector2.down);
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (_actions.Player.Left.triggered)
         {
             transform.rotation = Quaternion.Euler(0, 0, 90);
             PlayerMove(Vector2.left);
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (_actions.Player.Right.triggered)
         {
             transform.rotation = Quaternion.Euler(0, 0, -90);
             PlayerMove(Vector2.right);
@@ -50,7 +68,23 @@ public class FrogController : MonoBehaviour
         canMove = false;
         _anim.SetTrigger("Hop");
         Vector2 _destination = transform.position + (Vector3)_direction;
-        StartCoroutine(LerpMove(_destination));
+
+        Collider2D _platform = Physics2D.OverlapBox(_destination, 
+                                                    Vector2.zero, 
+                                                    0, 
+                                                    LayerMask.GetMask("Platform"));
+        if (_platform != null)
+        {
+            transform.SetParent(_platform.transform);
+            onPlatform = true;
+        }
+        else
+        {
+            transform.SetParent(null);
+            onPlatform = false;
+        }
+
+            StartCoroutine(LerpMove(_destination));
     }
 
     IEnumerator LerpMove(Vector2 _destination)
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public GameObject playerPrefab;
+    public Transform startPoint;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (Instance != null)
+        {
+            Destroy(Instance);
+        }
+        else
+        {
+            Instance = this;
+            startPoint = GameObject.FindGameObjectWithTag("StartPoint").transform;
+        }
+    }
+
+    public void SpawnFrog()
+    {
+        Instantiate(playerPrefab, startPoint.position, startPoint.rotation);
+    }
+}
